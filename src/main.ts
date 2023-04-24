79f33a05408997df9ab6e1ba1a9271a3c889cfdf
Bruno Paulin <bruno.p-@hotmail.com>
2023-04-23 23:19:13 -0300

    feat: add use.cors to main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(cors());
   app.useGlobalPipes(
     new ValidationPipe({ whitelist: true }),
     new ValidationPipe({
