dbdd9ccbcbdebf90e0e879a5c8d464b0590512b2
roger <aguiarroger02@gmail.com>
2023-05-07 16:34:57 -0300

    feat: add swagger docs

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -15,6 +16,15 @@ async function bootstrap() {
       },
     }),
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('Motors Shop API')
+    .setDescription('Documentação oficial da API do Motors Shop')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3001);
 }
 bootstrap();
